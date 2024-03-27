9bf5340106083b588fdf72e21f7ac40905f7b61e
nattan <fnatanieljunior@gmail.com>
2024-03-27 19:02:52 -0300

    projeto iniciado, titulos e paragrafos postos, imagens importadas.

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from './components'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ my-app/src/components/index.jsx
@@ -0,0 +1,26 @@
+import '../components/index.css'
+import ImageSofa from '../assets/image2.png'
+import GifSofa from '../assets/Sofá.gif'
+
+export default function Card(){
+
+return (
+    <>
+
+     <div className='container'>
+      <div className='Images'>
+
+      </div>
+
+       <div className='details'>
+        <p>CÓDIGO: 42404</p>
+        <h1>sofá Margot II - Rosé</h1>
+        <p>R$ 4.000</p>
+        <button> ADICIONA À CESTA</button>
+       </div>
+     </div> 
+
+    
+    </>
+)
+}
